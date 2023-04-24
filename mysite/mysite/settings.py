fcebddec788cccea57cc9114f8cfd88071bfa678
cjoaks <connorjoaks@gmail.com>
2023-04-24 14:06:42 -0500

    whitenoise with staticfiles this time

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic', 
     'mysite',
     'compressor'
 ]
